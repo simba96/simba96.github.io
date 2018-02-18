d18a3df76cb02bd97d191e03d2822e2c7585968c
Andrey <makelale96@gmail.com>
2018-02-18 12:49:31 +0300

    first commit

---
+++ dist/js/data.js
@@ -0,0 +1,36 @@
+var defaultData =  {
+		cats: [
+		  {id: 1,  title:'Smoll'},
+		  {id: 2,  title:'Big'}
+		],
+		products : [
+				{ id: 1, cid: 1, title: 'Kakarik Lutino',  im:  '12032500_w200_h200_4010234969w640h640fonkakar.jpg', price: 3000 },
+				{ id: 2, cid: 1, title: 'Kakarik Cinnamon ', im : '15600963_w200_h200_bff2b3fd3f8968ab82383cde76.jpg', price: 500 },
+				{ id: 3, cid: 1, title: 'Kalita Monk', im : '15641703_w200_h200_dsc03131.jpg', price: 2500 },
+				{ id: 4, cid: 1, title: 'Kalita Quaker', im : '16125335_w200_h200_dsc03111.jpg', price: 2500 },
+				{ id: 5, cid: 1, title: 'Kakarik New Zealand', im :  '18985647_w200_h200_dsc01823.jpg', price: 500 },
+				{ id: 6, cid: 1, title: 'Corella Nymph', im : '18985834_w200_h200_dsc02677.jpg', price: 500 },
+				{ id: 7, cid: 1, title: 'Kakarik Dominant', im : '18986829_w200_h200_dsc01768.jpg', price: 500 },
+				{ id: 8, cid: 1, title: 'Sunny Arating', im : '985882999_w200_h200_bccb3d8d_2d67___9466ca996.jpeg', price: 5000 },
+				{ id: 9, cid: 1, title: 'Burnt Green', im : '26842632_w200_h200_dsc05921.jpg', price: 3000 },
+				{ id: 10, cid: 1, title: 'Pionites melanocephala', im : '75843255_w200_h200_dsc07899.jpg', price: 3000 },
+				{ id: 11, cid: 1, title: 'Rosella Penantovaya', im : '199547042_w200_h200_img_2109.jpg', price: 2500 },
+                { id: 18, cid: 1, title: 'Caillac Blackhead', im : '124939743_w200_h200_image8.jpg', price: 2500 },
+				{ id: 12, cid: 2, title: 'Amazona Ochrocephala', im : '55199442_w200_h200_dsc07202.jpg', price: 4500 },
+				{ id: 13, cid: 2, title: 'Amazona Autumnalis', im : '55197801_w200_h200_dsc07192.jpg', price: 4500 },
+				{ id: 14, cid: 2, title: 'Eolophus Roseicapilla', im: '199545986_w200_h200_img_2121.jpg', price: 5000 },
+				{ id: 15, cid: 2, title: 'Jaco', im : '15755673_w200_h200_dsc02000.jpg', price: 5000 },
+				{ id: 16, cid: 2, title: 'Psittacus Erithacus', im : '43159641_w200_h200_dsc07099.jpg', price: 5000 },
+				{ id: 17, cid: 2, title: 'Amazona Aestiva', im : '83758509_w200_h200_img0038.jpg', price: 4500 }
+				// { id: 19, cid: 4, title: 'Кете с мясом баранины', im : 'im19.jpg', price: 60 },
+				// { id: 20, cid: 4, title: 'Кюрза жареная', im : 'im20.jpg', price: 85 },
+				// { id: 21, cid: 5, title: 'Лаваш с сулугуни и зеленью', im : 'im21.jpg', price: 78 },
+				// { id: 22, cid: 5, title: 'Шашлык из куриного филе', im : 'im22.jpg', price: 160 },
+				// { id: 23, cid: 5, title: 'Дорадо', im : 'im23.jpg', price: 295 },
+				// { id: 24, cid: 5, title: 'Антрекот из телятины ', im : 'im24.jpg', price: 125 },
+				// { id: 25, cid: 6, title: 'Мясной сет мангал', im : 'im25.jpg', price: 958 },
+				// { id: 26, cid: 6, title: 'Рыбный сет', im : 'im26.jpg', price: 988 },
+		]
+};
+
+
