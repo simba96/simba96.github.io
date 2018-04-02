0165b945be71d62ff6b4deb9630440b558080cf6
Andrey <makelale96@gmail.com>
2018-04-02 11:35:10 +0300

    add

--- Imperial/js/script.js
+++ Imperial/js/script.js
@@ -163,3 +163,19 @@ $(function () {
         $(this).next().slideToggle('slow').siblings('p:visible').slideUp('slow');
     });
 });
+
+jQuery(document).ready(function(){
+    $objWindow = $(window);
+    $('div[data-type="background"]').each(function(){
+        var $bgObj = $(this);
+        $(window).scroll(function() {
+            var yPos = -($objWindow.scrollTop() / $bgObj.data('speed'));
+
+            var coords = '100% '+ yPos + 'px';
+            // Animate the background
+            $bgObj.css({ backgroundPosition: coords });
+
+        });
+
+    });
+});
